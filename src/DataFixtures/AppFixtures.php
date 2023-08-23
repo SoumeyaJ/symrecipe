9edb9cfec0114b764883816b6b4d098819701e2e
mourad <sandyyousfival@gmail.com>
2023-08-23 14:57:28 +0200

    :construction: Login Form

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,12 +3,13 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\User;
 use Faker\Generator;
 use App\Entity\Recipe;
 use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -17,9 +18,12 @@ class AppFixtures extends Fixture
      */
     private Generator $faker;
 
+    // private UserPasswordHasherInterface $hasher;
+
     public function __construct()
     {
         $this->faker = Factory::create('fr_FR');
+        // $this->hasher = $hasher;
     }
 
     public function load(ObjectManager $manager): void
@@ -51,6 +55,22 @@ class AppFixtures extends Fixture
                 $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
             }
             $manager->persist($recipe);
+        }
+
+        //Users
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setFullName($this->faker->name())
+                ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() : null)
+                ->setEmail($this->faker->email())
+                ->setRoles(['ROLE USER'])
+                // -> setPassword('password')
+                ->setPlainPassword('password');
+            
+            // $hashPassword = $this->hasher->hashPassword($user, 'password');
+            // $user->setPassword($hashPassword);    
+           
+            $manager->persist($user);
         }
 
         $manager->flush();
